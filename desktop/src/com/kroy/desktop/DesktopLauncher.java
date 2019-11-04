54bcc8447429b147b1598df7c20fe766266e3ca5
Archie <archiegod@gmail.com>
2019-11-04 23:11:07 +0000

    Started implementing project

---
+++ desktop/src/com/kroy/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kroy.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kroy.Kroy;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Kroy(), config);
+	}
+}
