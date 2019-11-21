b0329be531745aa8446dcac21a23d58a940d1700
Archie <archiegod@gmail.com>
2019-11-21 23:04:02 +0000

    Added example test class

--- core/src/test/KroyTests.java
+++ core/src/test/KroyTests.java
@@ -1,17 +1,70 @@
 package test;
 
+// Java File type import
+import java.io.File;
+
+// JUnit imports
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+// Class imports
 import com.kroy.Kroy;
 
 class KroyTests {
 
-    private final Kroy kroy = new Kroy();
+    // Allows test class to be acessed in all methods
+    private static Kroy TestClass;
 
+    // Runs before all tests
+    @BeforeAll
+    public static void setUp() {
+        // Create a new Kroy object to test
+        TestClass = new Kroy();
+    }
+     
+    // Checks that the class contains all the required methods
     @Test
-    void example() {
-        assertEquals(kroy.getClass().getName(), kroy.getClass().getName());
+    public void hasCorrectMethods() {
+
+        // All methods that should be in the class
+        String[] correctMethods = {"create", "render", "dispose"};
+
+        for (int i = 0; i < correctMethods.length; i++) {
+            try {
+                // JUnit test to see if it exists
+                assertEquals(correctMethods[i], TestClass.getClass().getMethod(correctMethods[i]).getName());
+            } catch (NoSuchMethodException | SecurityException e) {
+                fail("Class does not contain " + correctMethods[i] + " method");
+            }
+        }
     }
 
+    // Checks that all the assets the class uses exist
+    @Test
+    public void hasAssets() {
+
+        // All methods that chould be in the class
+        String[] correctAssets = {"badlogic.jpg"};
+
+        for (int i = 0; i < correctAssets.length; i++) {
+
+            // Create file object from filepath
+            File file = new File("assets/" + correctAssets[i]);
+
+            // Check if file exists
+            if (!file.exists() || !file.getAbsolutePath().contains("assets/" + correctAssets[i])){
+                fail("Missing asset " + correctAssets[i]);
+            }
+        }   
+    }
+
+    // Runs after all tests, perform any clean up here
+    @AfterAll
+    public static void cleanUp() {
+        // Add here if neccessary
+    }
 }
