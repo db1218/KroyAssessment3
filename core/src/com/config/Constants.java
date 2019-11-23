31bfad95958a3989cb05442b1c341e9a246eb3c7
Archie <archiegod@gmail.com>
2019-11-23 12:17:50 +0000

    Added constants file

---
+++ core/src/com/config/Constants.java
@@ -0,0 +1,13 @@
+package com.config;
+
+// Allows access to constants throughout project
+public final class Constants {
+
+    private Constants() {
+        // Any constants that need instantiation go here
+    }
+
+    public static final String GAME_NAME = "Kroy";
+    public static final int SCREEN_WIDTH = 1280;
+    public static final int SCREEN_HEIGHT = 720;
+}
--- desktop/src/com/kroy/desktop/DesktopLauncher.java
+++ desktop/src/com/kroy/desktop/DesktopLauncher.java
@@ -1,5 +1,10 @@
 package com.kroy.desktop;
 
+// Constants import
+import static com.config.Constants.GAME_NAME;
+import static com.config.Constants.SCREEN_HEIGHT;
+import static com.config.Constants.SCREEN_WIDTH;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.kroy.Kroy;
@@ -7,9 +12,9 @@ import com.kroy.Kroy;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Kroy";
-		config.width = 1280;
-		config.height = 720;
+		config.title = GAME_NAME;
+		config.width = SCREEN_WIDTH;
+		config.height = SCREEN_HEIGHT;
 		new LwjglApplication(new Kroy(), config);
 	}
 }
