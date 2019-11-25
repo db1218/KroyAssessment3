a4275540f6d589e5608dcec06cb5c73fdf0a84a6
Archie <archiegod@gmail.com>
2019-11-25 22:52:38 +0000

    Linked assets and added image sources

--- MapAssets/TileSheets/Buildings Exterior.tsx
+++ MapAssets/TileSheets/Buildings Exterior.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="Buildings Exterior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../Assets/toUse/build_atlas.png" width="1024" height="1024"/>
+ <image source="../TileSheetSources/build_atlas.png" width="1024" height="1024"/>
 </tileset>
--- MapAssets/TileSheets/Terrain.tsx
+++ MapAssets/TileSheets/Terrain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="Terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <image source="../Assets/toUse/Terrain and Outside.png" width="1024" height="1024"/>
+ <image source="../TileSheetSources/Terrain and Outside.png" width="1024" height="1024"/>
 </tileset>
