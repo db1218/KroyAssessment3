3d987449859ca87469b4b1dee35492087a6559b0
Archie <archiegod@gmail.com>
2019-12-28 18:32:18 +0000

    Changed map to use "upLeft" coords

--- core/assets/MapAssets/TileSheets/galletcity_tiles.tsx
+++ core/assets/MapAssets/TileSheets/galletcity_tiles.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.1" name="galletcity_tiles" tilewidth="8" tileheight="8" tilecount="168" columns="8">
- <image source="../../../../../../Kroy/core/assets/MapAssets/TileSheetSources/galletcity_tiles.png" width="64" height="168"/>
+ <image source="../TileSheetSources/galletcity_tiles.png" width="64" height="168"/>
+ <tile id="0">
+  <properties>
+   <property name="BLOCKED" value=""/>
+  </properties>
+ </tile>
 </tileset>
