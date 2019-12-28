ce2003fba6d21567669bfa96add472640912a068
Arr0gant <albertoarjanlee@gmail.com>
2019-12-28 15:29:02 +0000

    Creation of new map+adding sprite sheet

---
+++ core/assets/MapAssets/TileSheets/galletcity_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="galletcity_tiles" tilewidth="8" tileheight="8" tilecount="168" columns="8">
+ <image source="../../../../../../Kroy/core/assets/MapAssets/TileSheetSources/galletcity_tiles.png" width="64" height="168"/>
+</tileset>
