2a4c85053350f93f81d4afda8ea57ac9aceb8fed
Josh <joshua@levett.info>
2020-01-12 18:02:00 +0000

    Add test files for Projectile and ResourceBar classes

---
+++ core/test/com/classes/ProjectileTest.java
@@ -0,0 +1,57 @@
+package com.classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//Import Kroy game
+import com.kroy.Kroy;
+import com.classes.Projectile;
+
+/**
+ * @author Joshua
+ *
+ */
+class ProjectileTest {
+
+	/**
+	 * Test method for {@link com.classes.Projectile#update(com.badlogic.gdx.graphics.g2d.Batch)}.
+	 */
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.Projectile#Projectile(com.badlogic.gdx.graphics.Texture, float, float)}.
+	 */
+	@Test
+	void testProjectile() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.Projectile#calculateTrajectory(com.badlogic.gdx.math.Polygon)}.
+	 */
+	@Test
+	void testCalculateTrajectory() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.Projectile#isOutOfView(com.badlogic.gdx.math.Vector3)}.
+	 */
+	@Test
+	void testIsOutOfView() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.Projectile#isOutOfMap()}.
+	 */
+	@Test
+	void testIsOutOfMap() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ core/test/com/classes/ResourceBarTest.java
@@ -0,0 +1,113 @@
+package com.classes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//Import Kroy game
+import com.kroy.Kroy;
+import com.classes.ResourceBar;
+
+/**
+ * @author Joshua
+ *
+ */
+class ResourceBarTest {
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#ResourceBar(float, float)}.
+	 */
+	@Test
+	void testResourceBar() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#update(com.badlogic.gdx.graphics.g2d.Batch)}.
+	 */
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#setPosition(float, float)}.
+	 */
+	@Test
+	void testSetPosition() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#setFade(boolean, boolean)}.
+	 */
+	@Test
+	void testSetFade() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#getFade()}.
+	 */
+	@Test
+	void testGetFade() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#setColourRange(com.badlogic.gdx.graphics.Color[])}.
+	 */
+	@Test
+	void testSetColourRange() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#getCurrentAmount()}.
+	 */
+	@Test
+	void testGetCurrentAmount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#getMaxAmount()}.
+	 */
+	@Test
+	void testGetMaxAmount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#setMaxResource(int)}.
+	 */
+	@Test
+	void testSetMaxResource() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#setResourcePercentage(int)}.
+	 */
+	@Test
+	void testSetResourcePercentage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#addResourceAmount(int)}.
+	 */
+	@Test
+	void testAddResourceAmount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.classes.ResourceBar#subtractResourceAmount(int)}.
+	 */
+	@Test
+	void testSubtractResourceAmount() {
+		fail("Not yet implemented");
+	}
+
+}
