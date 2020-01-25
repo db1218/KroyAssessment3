29db46aa3e4e5ba3b53f6f81f432bc012a615425
dan <db1218@york.ac.uk>
2020-01-25 12:55:50 +0000

    Started to edit the map a little, going to try our way of collisions

---
+++ core/assets/MapAssets/collisions.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="collisions" tilewidth="8" tileheight="8" tilecount="3600" columns="60">
+ <image source="collision_objects.png" width="480" height="480"/>
+</tileset>
