f8797e24922aa040d675cf276c621c533d901b31
Looby <emh566@york.ac.uk>
2020-02-11 17:59:43 +0000

    Auto stash before rebase of "origin/master"

--- core/src/com/screens/GameScreen.java
+++ core/src/com/screens/GameScreen.java
@@ -606,7 +606,7 @@ public class GameScreen implements Screen {
 			MinigameSprite minigameSprite = this.minigameSprites.get(i);
 			if (Intersector.overlapConvexPolygons(firetruck.getMovementHitBox(), minigameSprite.getMovementHitBox())) {
 				// open mini game
-				this.game.setScreen(new MinigameScreen(this.game));
+				this.game.setScreen(new MinigameScreen());
 				this.minigameSprites.remove(minigameSprite);
 			}
 		}
