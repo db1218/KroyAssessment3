77a0e1f20e812b81f843f36c6631bf9dd8888a81
Emilien <emilien.bev.com@gmail.com>
2020-02-15 22:36:10 +0000

    .

--- tests/src/com/sprites/PatrolMovementSpriteTest.java
+++ tests/src/com/sprites/PatrolMovementSpriteTest.java
@@ -29,7 +29,7 @@ public class PatrolMovementSpriteTest {
     private Texture mockSpriteTexture;
 
     private PatrolMovementSprite patrolMovementSpriteUnderTest;
-    
+
 
     @Before
     public void setUp() {
@@ -37,36 +37,28 @@ public class PatrolMovementSpriteTest {
         Array<Junction> junctions = new Array<>();
         junctions.add(new Junction(0,0, "junction 1"));
         junctions.add(new Junction(10,10, "junction 2"));
-       // when(mockMapGraph.getJunctions()).thenReturn(null);
-        GraphPath graphPathMock = Mockito.mock(GraphPath.class);
+        final Road road = new Road(new Junction(0.0f, 0.0f, "name"), new Junction(0.0f, 0.0f, "name"));
+        final GraphPath graphPathMock = Mockito.mock(GraphPath.class);
         when(graphPathMock.getCount()).thenReturn(2);
-//        when(mockMapGraph.findPath(junctions.get(0), junctions.get(1))).thenReturn(graphPathMock);
+        when(mockMapGraph.findPath(junctions.get(0), junctions.get(1))).thenReturn(graphPathMock);
         when(mockSpriteTexture.getHeight()).thenReturn(10);
         when(mockSpriteTexture.getWidth()).thenReturn(10);
+        when(mockMapGraph.getJunctions()).thenReturn(junctions);
+        when(mockMapGraph.getRoad(any(Junction.class), any(Junction.class))).thenReturn(road);
+        when(mockMapGraph.isRoadLocked(any(Junction.class), any(Junction.class))).thenReturn(false);
         patrolMovementSpriteUnderTest = new PatrolMovementSprite(mockSpriteTexture, mockMapGraph);
     }
 
     @Test
     public void testSetGoal() {
         final Junction goal = new Junction(0.0f, 0.0f, "name");
-        when(mockMapGraph.findPath(any(Junction.class), any(Junction.class))).thenReturn(null);
-
         patrolMovementSpriteUnderTest.setGoal(goal);
-
         assertEquals(patrolMovementSpriteUnderTest.getGoal(), goal);
     }
 
     @Test
     public void testStep() {
         final Array<Junction> junctions = new Array<>(false, new Junction[]{new Junction(0.0f, 0.0f, "name")}, 0, 0);
-        when(mockMapGraph.getJunctions()).thenReturn(junctions);
-
-        when(mockMapGraph.findPath(any(Junction.class), any(Junction.class))).thenReturn(null);
-        when(mockMapGraph.isRoadLocked(any(Junction.class), any(Junction.class))).thenReturn(false);
-
-        final Road road = new Road(new Junction(0.0f, 0.0f, "name"), new Junction(0.0f, 0.0f, "name"));
-        when(mockMapGraph.getRoad(any(Junction.class), any(Junction.class))).thenReturn(road);
-
         patrolMovementSpriteUnderTest.step();
 
         verify(mockMapGraph).unlockRoad(any(Road.class), any(PatrolMovementSprite.class));
